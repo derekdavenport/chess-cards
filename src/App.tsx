185c41efe9a1e88e565ba2240ca3a7fa0dd255a2
Derek Davenport <derekdavenport@gmail.com>
2023-10-12 12:13:46 -0400

    chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import a from './assets/a.tsv'
-import b from './assets/b.tsv'
-import c from './assets/c.tsv'
-import d from './assets/d.tsv'
-import e from './assets/e.tsv'
-
-type RawOpening = {
-	eco: string
-	epd: string
-	name: string
-	pgn: string
-	uci: string
-}
-
-type Openings = Record<string, RawOpening>
-
-const book = [a, b, c, d, e].reduce((acc: Openings, rawOpenings: RawOpening[]) => {
-	return rawOpenings.reduce((acc, rawOpening) => {
-		acc[rawOpening.uci] = rawOpening
-		return acc
-	}, acc)
-}, {})
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard"
+import type { Arrow } from "react-chessboard/dist/chessboard/types"
 
 type Results = {
 	white: number
@@ -36,9 +17,14 @@ type Move = Results & {
 
 type Data = Results & {
 	moves: Move[]
+	opening: {
+		eco: string
+		name: string
+	} | null
 }
 
 function App() {
+	const [game] = useState(new Chess())
 	const [moves, setMoves] = useState<Move[]>([])
 	const [data, setData] = useState<Data | null>(null)
 
@@ -46,32 +32,14 @@ function App() {
 	const uci = moves.map(move => move.uci)
 	const uciComma = uci.join(',')
 
-	// remove moves until we find a name
-	let uciSpace = ''
-	let i = uci.length
-	for (; !(uciSpace in book) && i > 0; i--) {
-		uciSpace = uci.slice(0, i).join(' ')
-	}
-	// i++
-	// const extraMoves = san.slice(i).reduce((extraMoves, san, j) => {
-	// 	if (i % 2) { // odd
-	// 		if (!j) { // first extra move
-	// 			extraMoves += `${(i - 1) / 2 + 1}. ...`
-	// 		}
-	// 	}
-	// 	else {
-	// 		extraMoves += `${i / 2 + 1}. `
-	// 	}
-	// 	extraMoves += san + ' '
-	// 	i++
-	// 	return extraMoves
-	// }, '')
+	//const uciSpace = uci.join(' ')
 
 	useEffect(() => {
 		(async function go() {
 			const response = await fetch(`https://explorer.lichess.ovh/masters?play=${uciComma}`)
 			if (response.ok) {
 				setData(await response.json())
+				// game.moves()
 			}
 		})()
 	}, [uciComma])
@@ -80,24 +48,43 @@ function App() {
 
 	return (
 		<>
-			<h1>Opening Explorer PoC</h1>
+			<h1 className="">Opening Explorer PoC</h1>
 			<p>
 				<strong>Moves:</strong> {san.map((move, i) => <>
-					{i % 2 ? ' ' : ` ${i / 2 + 1}.`} <span onClick={() => setMoves(moves.slice(0, i + 1))} style={{ cursor: 'pointer' }}>{move}</span>
+					{i % 2 ? ' ' : ` ${i / 2 + 1}.`} <span onClick={() => {
+						const nextMoves = moves.slice(0, i + 1)
+						setMoves(nextMoves)
+						game.reset()
+						nextMoves.forEach(move => game.move(move.san))
+					}} style={{ cursor: 'pointer' }}>{move}</span>
 				</>)}
 			</p>
 			<p>
-				<strong>Name:</strong> {book[uciSpace]?.eco} {book[uciSpace]?.name}
+				<strong>Name:</strong> {data?.opening && `${data?.opening.eco} ${data?.opening.name}`}
 			</p>
-			{moves.length ? <span onClick={() => setMoves(moves.slice(0, -1))} style={{ cursor: 'pointer' }}>..</span> : undefined}
-			<p>
-				<strong>Next Move:</strong>
-			</p>
-			<ul>
-				{data?.moves.map(move => {
-					return <li key={move.uci} onClick={() => setMoves([...moves, move])} style={{ cursor: 'pointer' }}>{Math.floor(n / 2) + 1}. {n % 2 ? '...' : ''}{move.san}</li>
-				})}
-			</ul>
+			<div className="flex">
+				<div className="flex-1">
+					{moves.length ? <span onClick={() => {
+						const nextMoves = moves.slice(0, -1)
+						setMoves(nextMoves)
+						game.undo()
+					}} style={{ cursor: 'pointer' }}>..</span> : undefined}
+					<p>
+						<strong>Next Move:</strong>
+					</p>
+					<ul className="list-disc list-inside">
+						{data?.moves.map(move => {
+							return <li key={move.uci} onClick={() => {
+								setMoves([...moves, move])
+								game.move(move.san)
+							}} style={{ cursor: 'pointer' }}>{Math.floor(n / 2) + 1}. {n % 2 ? '...' : ''}{move.san}</li>
+						})}
+					</ul>
+				</div>
+				<div className="flex-1">
+					<Chessboard position={game.fen()} customArrows={data?.moves.map(move => move.uci.split(/(?=..$)/) as Arrow)} />
+				</div>
+			</div>
 		</>
 	)
 }
