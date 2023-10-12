cdc61d4b274d999dcfe3b51af2484f9520e1fd3c
Derek Davenport <derekdavenport@gmail.com>
2023-10-11 23:55:07 -0400

    opening names and cloudflare deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,90 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import a from './assets/a.tsv'
+import b from './assets/b.tsv'
+import c from './assets/c.tsv'
+import d from './assets/d.tsv'
+import e from './assets/e.tsv'
+
+type RawOpening = {
+	eco: string
+	epd: string
+	name: string
+	pgn: string
+	uci: string
+}
+
+type Openings = Record<string, RawOpening>
+
+const book = [a, b, c, d, e].reduce((acc: Openings, rawOpenings: RawOpening[]) => {
+	return rawOpenings.reduce((acc, rawOpening) => {
+		acc[rawOpening.uci] = rawOpening
+		return acc
+	}, acc)
+}, {})
 
 type Results = {
-  white: number
-  draws: number
-  black: number
+	white: number
+	draws: number
+	black: number
 }
 
 type Move = Results & {
-  uci: string
-  san: string
+	uci: string
+	san: string
 }
 
 type Data = Results & {
-  moves: Move[]
+	moves: Move[]
 }
 
 function App() {
-  const [moves, setMoves] = useState<Move[]>([])
-  const [data, setData] = useState<Data | null>(null)
-  useEffect(() => {
-    (async function go() {
-      const response = await fetch(`https://explorer.lichess.ovh/masters?play=${moves.map(({uci}) => uci).join(',')}`)
-      if (response.ok) {
-        setData(await response.json())
-      }
-    })()
-  }, [moves])
+	const [moves, setMoves] = useState<Move[]>([])
+	const [data, setData] = useState<Data | null>(null)
 
-  return (
-    <>
-      <h1>Opening Explorer PoC</h1>
-      <p>
-        <strong>Moves:</strong> {moves.map(move => move.san).join(', ')}
-      </p>
-      {moves.length ? <span onClick={() => setMoves(moves.slice(0, -1))} style={{ cursor: 'pointer' }}>..</span> : undefined}
-      <p>
-        <strong>Next Move:</strong>
-      </p>
-      <ol>
-        {data?.moves.map(move => {
-          return <li key={move.uci} onClick={() => setMoves([...moves, move])} style={{ cursor: 'pointer' }}>{move.san}</li>
-        })}
-      </ol>
-    </>
-  )
+	const san = moves.map(move => move.san)
+	const uci = moves.map(move => move.uci)
+	const uciComma = uci.join(',')
+
+	// remove moves until we find a name
+	let extraMoves = ''
+	let uciSpace = ''
+	for (let i = uci.length; !(uciSpace in book) && i > 0; i--) {
+		uciSpace = uci.slice(0, i).join(' ')
+		extraMoves = san.slice(i).join(', ')
+	}
+
+	useEffect(() => {
+		(async function go() {
+			const response = await fetch(`https://explorer.lichess.ovh/masters?play=${uciComma}`)
+			if (response.ok) {
+				setData(await response.json())
+			}
+		})()
+	}, [uciComma])
+
+	return (
+		<>
+			<h1>Opening Explorer PoC</h1>
+			<p>
+				<strong>Moves:</strong> {san.map((move, i) => {
+					return <><span onClick={() => setMoves(moves.slice(0, i+1))} style={{ cursor: 'pointer' }}>{move}</span>{i % 2 && i < moves.length - 1 ? ',' : ''} </>
+				})}
+			</p>
+			<p>
+				<strong>Name:</strong> {book[uciSpace]?.eco} {book[uciSpace]?.name} {extraMoves ? `(+ ${extraMoves})` : ''}
+			</p>
+			{moves.length ? <span onClick={() => setMoves(moves.slice(0, -1))} style={{ cursor: 'pointer' }}>..</span> : undefined}
+			<p>
+				<strong>Next Move:</strong>
+			</p>
+			<ol>
+				{data?.moves.map(move => {
+					return <li key={move.uci} onClick={() => setMoves([...moves, move])} style={{ cursor: 'pointer' }}>{move.san}</li>
+				})}
+			</ol>
+		</>
+	)
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import dsv from '@rollup/plugin-dsv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills(),
+    dsv(),
   ],
-  build: {
-    outDir: 'docs',
-  },
 })
