db0b9855b36214e229598d60e9828bb0762d6dbb
Derek Davenport <derekdavenport@gmail.com>
2023-10-13 00:53:45 -0400

    arrow opacity

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ type Results = {
 type BookMove = Results & {
 	uci: string
 	san: string
+	averageRating: number
+	game: null
 }
 
 type Data = Results & {
@@ -26,56 +28,74 @@ type Data = Results & {
 const captureBG = 'radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 80%, rgba(20,85,30,0.3) 80%'
 const moveBG = 'radial-gradient(circle, rgba(20,85,30,0.3) 0%, rgba(20,85,30,0.3) 20%, rgba(0,0,0,0) 20%'
 
+function splitUci(uci: string) {
+	return [uci.slice(0, 2), uci.slice(2)] as [Square, Square]
+}
+
 function App() {
 	const [game] = useState(new Chess())
 	const [moves, setMoves] = useState<Pick<BookMove, 'uci' | 'san'>[]>([])
-	const [data, setData] = useState<Data | null>(null)
+	const [data, setData] = useState<Data>()
 
 	const san = moves.map(move => move.san)
 	const uci = moves.map(move => move.uci)
 	const uciComma = uci.join(',')
 
 	//const uciSpace = uci.join(' ')
-
+	// https://lichess.org/api/cloud-eval?fen=rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2
 	useEffect(() => {
 		(async function go() {
 			const response = await fetch(`https://explorer.lichess.ovh/masters?play=${uciComma}`)
 			if (response.ok) {
 				const data = await response.json() as Data
 				setData(data)
-				setCustomArrows(data.moves.map(move => move.uci.split(/(?=..$)/) as Arrow))
+				console.log(data.moves)
+				const timesPlayed = data.moves.map(move => move.white + move.draws + move.black)
+				const max = timesPlayed[0]
+				const min = timesPlayed[timesPlayed.length - 1]
+				const scale = (x: number) => .2 + (x - min) * (1 - .2) / (max - min) || 1
+				setCustomArrows(data.moves.map((move, i) => {
+					// eslint-disable-next-line prefer-const
+					let [startSquare, endSquare] = splitUci(move.uci)
+					// board does not allow castling to corners, must be input as moving 2 spaces
+					if (move.san == 'O-O') {
+						endSquare = 'g' + endSquare[1] as Square
+					}
+					else if(move.san == 'O-O-O') {
+						endSquare = 'c' + endSquare[1] as Square
+					}
+					return [startSquare, endSquare, `rgba(0,48,136,${scale(timesPlayed[i])})`]
+				}))
 				// game.moves()
 			}
 		})()
 	}, [uciComma])
 
-	const n = moves.length
-
 	const [selectedSquare, setSelectedSquare] = useState<Square>()
-	const [legalMoves, setLegalMoves] = useState<Move[]>([])
-	const [legalSquare, setLegalSquare] = useState<Square | undefined>()
-	const [customArrows, setCustomArrows] = useState<Arrow[] | undefined>()
+	const [legalSquare, setLegalSquare] = useState<Square>()
+	const [customArrows, setCustomArrows] = useState<Arrow[]>()
+	const legalMoves: Move[] = selectedSquare ? game.moves({ square: selectedSquare, verbose: true }) : []
+
 	const customSquareStyles: CustomSquareStyles = {}
-	// 	...legalMoves.reduce((styles, square) => {
-	// 		styles[square] = { background: 'radial-gradient(circle, rgba(20,85,30,0.3) 0%, rgba(20,85,30,0.3) 20%, rgba(0,0,0,0) 20%' }
-	// 		return styles
-	// 	}, ({} as CustomSquareStyles)),
-	// }
 	if (selectedSquare) {
 		customSquareStyles[selectedSquare] = { background: 'rgba(20,85,30,.5)' }
 	}
 	// show previous move
 	if (moves.length) {
-		customSquareStyles[moves[moves.length - 1].uci.slice(0, 2) as Square] = { background: 'rgba(155,199,0,.41)' }
-		customSquareStyles[moves[moves.length - 1].uci.slice(2) as Square] = { background: 'rgba(155,199,0,.41)' }
+		const previousMove = moves[moves.length - 1]
+		const [startSquare, endSquare] = splitUci(previousMove.uci)
+		customSquareStyles[startSquare] = { background: 'rgba(155,199,0,.41)' }
+		customSquareStyles[endSquare] = { background: 'rgba(155,199,0,.41)' }
 	}
 	for (const legalMove of legalMoves) {
-		// console.log('legalMove', legalMove)
 		customSquareStyles[legalMove.to] = { background: legalMove.captured ? captureBG : moveBG }
 	}
-	if (legalSquare) {
+	if (selectedSquare && legalSquare) {
 		customSquareStyles[legalSquare] = { background: 'rgba(20,85,30,0.3)' }
 	}
+	// TODO: draw check
+
+	const n = moves.length
 
 	return (
 		<>
@@ -86,7 +106,6 @@ function App() {
 						const nextMoves = moves.slice(0, i + 1)
 						setMoves(nextMoves)
 						setSelectedSquare(undefined)
-						setLegalMoves([])
 						game.reset()
 						nextMoves.forEach(move => game.move(move.san))
 					}} style={{ cursor: 'pointer' }}>{move}</span>
@@ -111,7 +130,6 @@ function App() {
 							<li key={move.uci} onClick={() => {
 								setMoves([...moves, move])
 								setSelectedSquare(undefined)
-								setLegalMoves([])
 								game.move(move.san)
 							}} style={{ cursor: 'pointer' }}>{Math.floor(n / 2) + 1}. {n % 2 ? '...' : ''}{move.san}</li>
 						)}
@@ -121,41 +139,35 @@ function App() {
 					<Chessboard
 						position={game.fen()} customArrows={customArrows}
 						onSquareClick={(square) => {
-							// console.log(selectedSquare, legalMoves, square)
 							if (square == selectedSquare) {
 								setSelectedSquare(undefined)
-								//setCustomArrows(data?.moves.map(move => move.uci.split(/(?=..$)/) as Arrow))
-								setLegalMoves([])
 							}
 							else {
 								if (game.turn() == game.get(square).color) {
 									setSelectedSquare(square)
-									const moves = game.moves({ square, verbose: true })
-									//setCustomArrows(moves.map(move => [move.from, move.to]))
-									setLegalMoves(moves.map(move => move))
 								}
 								// click move
-								else if (selectedSquare && legalMoves.find(move => move.to == square) !== undefined) {
+								else if (selectedSquare && legalMoves.some(move => move.to == square)) {
 									const result = game.move({ from: selectedSquare, to: square })
 									setMoves([...moves, { uci: result.lan, san: result.san }])
 									setSelectedSquare(undefined)
-									setLegalMoves([])
 									setLegalSquare(undefined)
 								}
 								else {
 									setSelectedSquare(undefined)
-									//setCustomArrows(data?.moves.map(move => move.uci.split(/(?=..$)/) as Arrow))
-									setLegalMoves([])
 								}
 							}
 						}}
 						onMouseOverSquare={(square) => {
-							if (legalMoves.find(move => move.to == square) !== undefined) {
+							if (legalMoves.some(move => move.to == square)) {
 								setLegalSquare(square)
 							}
 							else {
 								setLegalSquare(undefined)
 							}
+						}}
+						onMouseOutSquare={() => {
+							setLegalSquare(undefined)
 						}}
 						isDraggablePiece={({piece}) => {
 							//console.log(piece, sourceSquare)
@@ -166,12 +178,9 @@ function App() {
 						onPieceDragBegin={(_piece, sourceSquare) => {
 							// console.log('onPieceDragBegin', piece, sourceSquare)
 							setSelectedSquare(sourceSquare)
-							const moves = game.moves({ square: sourceSquare, verbose: true })
-							//setCustomArrows(moves.map(move => [move.from, move.to]))
-							setLegalMoves(moves)
 						}}
 						onDragOverSquare={(square) => {
-							if (legalMoves.find(move => move.to == square) !== undefined) {
+							if (legalMoves.some(move => move.to == square)) {
 								setLegalSquare(square)
 							}
 							else {
@@ -189,13 +198,11 @@ function App() {
 								const result = game.move({ from: sourceSquare, to: targetSquare })
 								setMoves([...moves, { uci: result.lan, san: result.san }])
 								setSelectedSquare(undefined)
-								setLegalMoves([])
 								setLegalSquare(undefined)
 								return true
 							}
 							catch (e) {
 								setSelectedSquare(undefined)
-								setLegalMoves([])
 								setLegalSquare(undefined)
 								return false
 							}
