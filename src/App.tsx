cd1599bb0853c75f4d3200e4e24aa40ea4ffc9e7
Derek Davenport <derekdavenport@gmail.com>
2023-10-31 23:58:49 -0400

    added images

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,11 @@ function App() {
 			return setLichessStudyUrlMessage('not a Lichess Study address')
 		}
 		const id = match[3]
-		const response = await fetch(`https://lichess.org/api/study/${id}.pgn`, {
+		const responsePromise = fetch(`https://lichess.org/api/study/${id}.pgn`, {
 			method: 'HEAD',
 		})
+		setLichessStudyUrlMessage(undefined)
+		const response = await responsePromise
 		if (!response.ok) {
 			return setLichessStudyUrlMessage('no Lichess Study at that address')
 		}
@@ -43,21 +45,41 @@ function App() {
 	}
 
 	const [lichessStudyId, setLichessStudyId] = useState<string>('')
-	const [lichessStudyUrlMessage, setLichessStudyUrlMessage] = useState('paste in a Lichess Study address')
+	const [lichessStudyUrlMessage, setLichessStudyUrlMessage] = useState<string | undefined>('paste in a Lichess Study address')
 	const [orientation, setOrientation] = useState('')
 	const studyInputRef = useRef<HTMLInputElement | null>(null)
 	const downloadLinkRef = useRef<HTMLAnchorElement | null>(null)
 	return <>
-		<main className="container my-0 mx-auto max-w-md text-center prose">
-			<h1 className="my-10"><img src="/chess-cards.png" className="inline max-h-20 m-0 rounded-lg"/> Chess Cards</h1>
-			<div className="flex flex-col space-y-2">
+		<main className="container text-center m-auto">
+			<header className="prose m-auto">
+				<h1 className="my-10"><img src="/chess-cards.png" className="inline max-h-20 m-0 rounded-lg" /> Chess Cards</h1>
+			</header>
+
+			<div className="flex items-center space-x-4 max-w-screen-md m-auto">
 				<div>
-					Pick a <a href="https://lichess.org/study">Lichess Study</a> and paste the address below
+					From Lichess Study
+					<img src="/lichess.png" className="h-auto" />
 				</div>
+				<div>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+					</svg>
+				</div>
+
+				<div>
+					To Anki Deck
+					<img src="/anki.png" className="h-auto" />
+				</div>
+			</div>
+
+			<div className="flex flex-col my-8 space-y-2">
+				<p>
+					Choose a <a href="https://lichess.org/study">Lichess Study</a> and paste the address below
+				</p>
 				<div className="form-control w-full max-w-xs mx-auto">
 					<label className="label" htmlFor="lichessStudyUrl">
 						<span className="label-text">Lichess Study Address</span>
-						<span className="label-text-alt text-red-500">{lichessStudyUrlMessage}</span>
+						<span className="label-text-alt text-red-500">{lichessStudyUrlMessage ?? <span className="loading loading-spinner loading-xs text-primary" />}</span>
 					</label>
 					<input
 						id="lichessStudyUrl" name="lichessStudyUrl" type="text" ref={studyInputRef}
@@ -88,18 +110,25 @@ function App() {
 						className="btn btn-primary"
 					>download</button>
 				</div>
+
 				<p>
 					Study using<br />
-					<a href="https://apps.ankiweb.net/">Anki Desktop</a>, <a href="https://ankiweb.net/decks">Anki Web</a>, <a href="https://play.google.com/store/apps/details?id=com.ichi2.anki">AnkiDroid</a>, or <a href="https://apps.apple.com/us/app/ankimobile-flashcards/id373493387">AnkiMobile Flashcards</a>
+					<a href="https://apps.ankiweb.net/#download">Anki Desktop</a>, <a href="https://ankiweb.net/decks">Anki Web</a>, <a href="https://play.google.com/store/apps/details?id=com.ichi2.anki">AnkiDroid</a>, or <a href="https://apps.apple.com/us/app/ankimobile-flashcards/id373493387">AnkiMobile Flashcards</a>
 				</p>
-				<div>
-					<h3 className="mt-20">Support Further Development</h3>
+
+				<div className="prose m-auto">
+					<h3 className="mt-10">Further Development</h3>
 					<p>
-						<a role="button" className="btn btn-secondary" href="https://donate.stripe.com/test_14k29GalRfOC9qwaEE">Donate</a>
+						Support this project <a role="button" className="btn btn-secondary" href="https://donate.stripe.com/fZe17v5kN6vQaIMfYY">Donate</a>
 					</p>
+					<h4>Ideas</h4>
+					<ul className="text-left">
+						<li>Import from other sources such as private studies, ChessBase, and PGN</li>
+						<li>Variations on playing side</li>
+					</ul>
 				</div>
 			</div>
-		</main>
+		</main >
 		<a download ref={downloadLinkRef}></a>
 	</>
 }
