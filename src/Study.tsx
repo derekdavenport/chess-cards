20fc45d60aad45aa5f9e58f23175d92fc5794b8a
Derek Davenport <derekdavenport@gmail.com>
2025-10-23 22:39:46 -0400

    start trying to build study pgn

--- src/Study.tsx
+++ src/Study.tsx
@@ -1,5 +1,8 @@
-import { atom, useAtom } from "jotai"
+import { atom, ExtractAtomValue, useAtom } from "jotai"
 import { atomWithQuery } from "jotai-tanstack-query"
+import { Chess, FEN, Event } from "cm-chess";
+import { useState } from "react";
+import { QueryClient, useQueryClient } from "@tanstack/react-query";
 
 interface PositionStats {
 	white: number,
@@ -41,57 +44,239 @@ interface Player {
 
 type DB = 'masters' | 'lichess' | 'player'
 const dbAtom = atom<DB>('masters')
-const uciListAtom = atom<string[]>([])
+//const uciListAtom = atom<string[]>([])
 const uciAtom = atom((get) => get(uciListAtom).join(','))
-const movesAtom = atomWithQuery((get) => ({
-  queryKey: ['explorer', get(dbAtom), get(uciAtom)],
-  queryFn: async ({ queryKey: [, db, uci] }) => {
-    const res = await fetch(`https://explorer.lichess.ovh/${db}?play=${uci}`)
-    return res.json() as Promise<ExplorerResponse>
-  },
+type ExplorerQueryKey = ['explorer', ExtractAtomValue<typeof dbAtom>, ExtractAtomValue<typeof uciAtom>]
+async function explorerQueryFn({ queryKey: [, db, uci] }: { queryKey: ExplorerQueryKey }) {
+	const res = await fetch(`https://explorer.lichess.ovh/${db}?play=${uci}`)
+	return res.json() as Promise<ExplorerResponse>
+}
+const movesAtom = atomWithQuery<ExplorerResponse, Error, ExplorerResponse, ExplorerQueryKey>(get => ({
+	queryKey: ['explorer', get(dbAtom), get(uciAtom)],
+	queryFn: explorerQueryFn,
+	staleTime: Infinity,
 }))
+
+interface Pv {
+	moves: string,
+	cp: number,
+}
+interface AnalysisResponse {
+	fen: string,
+	knodes: number,
+	depth: number,
+	pvs: Pv[],
+}
+type AnalysisQueryKey = ['analysis', ExtractAtomValue<typeof fenAtom>]
+const analysisAtom = atomWithQuery<AnalysisResponse, Error, AnalysisResponse, AnalysisQueryKey>((get) => ({
+	queryKey: ['analysis', get(fenAtom)],
+	queryFn: async ({ queryKey: [, fen] }) => {
+		const res = await fetch(`https://lichess.org/api/cloud-eval?fen=${encodeURIComponent(fen)}&multiPv=5`)
+		return res.json()
+	},
+	staleTime: Infinity,
+}))
+
+const nextMoveEvalsAtom = atom<{ [uci: string]: number }>((get) => {
+	const analysis = get(analysisAtom).data
+	if (!analysis) return {}
+	return analysis.pvs.reduce((evals, pv) => {
+		const uci = pv.moves.split(' ')[0]
+		return {
+			...evals,
+			[uci]: pv.cp / 100
+		}
+	}, {})
+})
+
+const game = new Chess()
+// Stash in object to make 'mutable'. Remember to set after any changes.
+const gameAtom = atom<{ game: Chess }>({ game })
+// const gameSignalAtom = atom<Event>({ type: 'initialized', fen: game.fen() })
+// gameSignalAtom.onMount = set => game.addObserver((event) => {
+// 	set(event)
+// 	console.log('game event', event)
+// })
+
+//const gameAtom = atom(() => ({ game, lastMove: game.lastMove() })) //{ fen: FEN.start })
+const fenAtom = atom<string>(get => get(gameAtom).game.fen())
+
+const uciListAtom = atom<string[]>(get => get(gameAtom).game.history().map(move => move.uci))
+
+
+
+const addMoveAtom = atom(null, (get, set, san: string) => {
+	const { game } = get(gameAtom)
+	const move = game.move(san)
+	set(gameAtom, { game })
+	// console.log('added move', san, move, game.fen())
+})
+
+const undoMoveAtom = atom(null, (get, set) => {
+	const { game } = get(gameAtom)
+	game.undo()
+	set(gameAtom, { game })
+})
+
+async function buildPGN(queryClient: QueryClient, startingFen: string, db: DB, maxDepth: number, variationsCount: number, playedPercent: number, bestMovesCount: number): string {
+	const game = new Chess(startingFen)
+	//game.history().map(move => move.uci).join
+
+	async function addMoves(fen: string, db: DB, depth: number, variationsCount: number, playedPercent: number) {
+		if (depth > maxDepth) return
+		const explorerPromise = queryClient.fetchQuery<ExplorerResponse, Error, ExplorerResponse, ExplorerQueryKey>({ queryKey: ['explorer', db, fen], queryFn: explorerQueryFn })
+		//, () => fetch(`https://explorer.lichess.ovh/${db}?fen=${encodeURIComponent(game.fen())}`))
+		const analysisPromise = fetch(`https://lichess.org/api/cloud-eval?fen=${encodeURIComponent(game.fen())}&multiPv=5`)
+		const analysisResponse = await analysisPromise
+		const explorer = await explorerPromise
+		const analysis: AnalysisResponse = await analysisResponse.json()
+		const nextMoveEvals: { [uci: string]: number } = analysis.pvs.reduce((evals, pv) => {
+			const uci = pv.moves.split(' ')[0]
+			return {
+				...evals,
+				[uci]: pv.cp / 100
+			}
+		}, {})
+		const previousMove = game.history()[game.history().length - 1]
+		explorer.moves.reduce((bestMoves, move) => {
+			if (bestMoves.length < bestMovesCount) {
+				const totalGames = move.white + move.draws + move.black
+				const winPercent = game.turn() === 'w' ? move.white / totalGames : move.black / totalGames
+				if (winPercent * 100 >= playedPercent) {
+					bestMoves.push(move)
+				}
+			}
+			return bestMoves
+		}, [] as Move[]).forEach(move => {
+			game.move(move, previousMove)
+		})
+
+	}
+
+	for (let i = 0; i < variationsCount; i++) {
+		const explorerResponse = await addMoves(game.fen(), db, 0, variationsCount, playedPercent)
+	}
+	
+}
 
 function Study() {
 	const [db, setDb] = useAtom(dbAtom)
-	const [uciList, setUciList] = useAtom(uciListAtom)
+	const [uciList] = useAtom(uciListAtom)
 	const [{data: moves}] = useAtom(movesAtom)
+	const [, addMove] = useAtom(addMoveAtom)
+	const [, undoMove] = useAtom(undoMoveAtom)
+	const [fen] = useAtom(fenAtom)
+	const [{data: analysis, isPending}] = useAtom(analysisAtom)
+	const [nextMoveEvals] = useAtom(nextMoveEvalsAtom)
+	const [commonMovesCount, setCommonMovesCount] = useState(2);
+	const [playedPercent, setPlayedPercent] = useState(25);
+	const [bestMovesCount, setBestMovesCount] = useState(1);
+	const [depth, setDepth] = useState(5);
 
-	
+	const queryClient = useQueryClient();
 
 	return <>
-		<select value={db} onChange={e => setDb(e.target.value as DB)} className="select select-primary">
-			<option value="masters">Masters</option>
-			<option value="lichess">Lichess</option>
-			<option value="player">Player</option>
-		</select>
+	{fen}<br />
 
+
+		Starting Position
 		{uciList.join(' ')}
 
 		<ol>
 			<li key="start" className="my-2">
 				<button onClick={() => {
-					setUciList([])
+
 				}} className="btn btn-sm btn-outline">
 					Start Position
 				</button>
 			</li>
 			<li className="my-2">
-				<button onClick={() => {
-					setUciList(uciList.slice(0, -1))
-				}} className="btn btn-sm btn-outline">
+				<button onClick={() => undoMove()} className="btn btn-sm btn-outline">
 					Undo Last Move
 				</button>
 			</li>
 			{moves && moves.moves.map((move) => (
 				<li key={move.uci} className="my-2">
-					<button onClick={() => {
-						setUciList([...uciList, move.uci])
-					}} className="btn btn-sm btn-outline">
+					<button onClick={() => { console.log(move); addMove(move.san) }}
+					className="btn btn-sm btn-outline">
 						{move.san} {move.opening && `${move.opening.eco} ${move.opening.name}`} (W:{move.white} D:{move.draws} B:{move.black})
+						{move.uci in nextMoveEvals && ` Eval: ${nextMoveEvals[move.uci].toFixed(2)}`}
 					</button>
 				</li>
 			))}
 		</ol>
+
+		Add moves
+		<fieldset className="fieldset  w-full max-w-xs mx-auto">
+			<legend className="fieldset-legend">Database Moves</legend>
+
+			<label className="label">Database</label>
+			<select value={db} onChange={e => setDb(e.target.value as DB)} className="select select-primary">
+				<option value="masters">Masters</option>
+				<option value="lichess">Lichess</option>
+				<option value="player">Player</option>
+			</select>
+
+			<label className="label">Variations</label>
+			<input
+				type="number"
+				className="input validator"
+				required
+				placeholder="Type a number between 1 to 10"
+				min="0"
+				max="10"
+				title="Must be between be 0 to 10"
+				value={commonMovesCount}
+				onChange={e => setCommonMovesCount(Number(e.target.value))}
+			/>
+			<p className="validator-hint">Must be between be 0 to 10</p>
+
+			<label className="label">Exclude Moves Played Less Than</label>
+			<div className="tooltip tooltip-bottom" data-tip={`${playedPercent}%`}>
+				<input type="range" min={0} max={50} value={playedPercent} className="range range-primary" onChange={e => setPlayedPercent(Number(e.target.value))} />
+			</div>
+			{/* <input
+				type="number"
+				className="input validator"
+				required
+				placeholder="Type a number between 0 to 50"
+				min="0"
+				max="50"
+				title="Must be between be 0 to 50"
+				value={playedPercent}
+				onChange={e => setPlayedPercent(Number(e.target.value))}
+			/> */}
+
+			<label className="label">Depth</label>
+			<input type="range" min={1} max="10" value={depth} className="range range-primary" onChange={e => setDepth(Number(e.target.value))} />
+		</fieldset>
+
+
+		<fieldset className="fieldset w-full max-w-xs mx-auto">
+			<legend className="fieldset-legend">Include Best Moves</legend>
+			<input
+				type="number"
+				className="input validator"
+				required
+				placeholder="Type a number between 1 to 10"
+				min="0"
+				max="5"
+				title="Must be between be 0 to 5"
+				value={bestMovesCount}
+				onChange={e => setBestMovesCount(Number(e.target.value))}
+			/>
+			<p className="validator-hint">Must be between be 0 to 5</p>
+		</fieldset>
+
+		<fieldset className="fieldset w-full max-w-xs mx-auto">
+			<legend className="fieldset-legend">My Move</legend>
+			<select className="select select-primary">
+				<option value="">Best Move</option>
+				<option value="">Most Common Move</option>
+			</select>
+		</fieldset>
+
+		<button onClick={() => buildPGN(queryClient)}>Go</button>
 	</>
 }
 
---
+++ src/cm-chess.d.ts
@@ -0,0 +1,228 @@
+declare module 'cm-chess' {
+	type Color = 'w' | 'b'
+	type PieceSymbol = 'p' | 'n' | 'b' | 'r' | 'q' | 'k'
+	type Piece = {
+		color: Color
+		type: PieceSymbol
+	}
+	type Square =
+		'a8' | 'b8' | 'c8' | 'd8' | 'e8' | 'f8' | 'g8' | 'h8' |
+		'a7' | 'b7' | 'c7' | 'd7' | 'e7' | 'f7' | 'g7' | 'h7' |
+		'a6' | 'b6' | 'c6' | 'd6' | 'e6' | 'f6' | 'g6' | 'h6' |
+		'a5' | 'b5' | 'c5' | 'd5' | 'e5' | 'f5' | 'g5' | 'h5' |
+		'a4' | 'b4' | 'c4' | 'd4' | 'e4' | 'f4' | 'g4' | 'h4' |
+		'a3' | 'b3' | 'c3' | 'd3' | 'e3' | 'f3' | 'g3' | 'h3' |
+		'a2' | 'b2' | 'c2' | 'd2' | 'e2' | 'f2' | 'g2' | 'h2' |
+		'a1' | 'b1' | 'c1' | 'd1' | 'e1' | 'f1' | 'g1' | 'h1'
+
+	type Move = {
+		captured?: boolean
+		color: Color
+		commentAfter: string | undefined
+		commentMove: string | undefined
+		fen: string
+		flags: string
+		from: Square
+		gameOver?: true
+		inCheck?: true
+		inCheckmate?: true
+		nag?: string
+		next: Move | undefined
+		piece: PieceSymbol
+		ply: number
+		promotion?: 'q' | 'r' | 'n' | 'b'
+		previous: Move | null
+		san: string
+		to: Square
+		uci: string
+		variation: Move[]
+		variations: Move[][]
+	}
+
+	type FEN = {
+		empty: '8/8/8/8/8/8/8/8 w - - 0 1',
+		start: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+	}
+	const FEN: FEN
+
+	const EVENT_TYPE = {
+		illegalMove: "illegalMove",
+		legalMove: "legalMove",
+		undoMove: "undoMove",
+		initialized: "initialized"
+	} as const
+
+	type ILLEGAL_MOVE_EVENT = { type: EVENT_TYPE.illegalMove, move: Move, previousMove?: Move }
+	type LEGAL_MOVE_EVENT = { type: EVENT_TYPE.legalMove, move: Move, previousMove?: Move }
+	type UNDO_MOVE_EVENT = { type: EVENT_TYPE.undoMove, move: Move }
+	type INITIALIZED_EVENT = { type: EVENT_TYPE.initialized, fen: string }
+	type Event = ILLEGAL_MOVE_EVENT | LEGAL_MOVE_EVENT | UNDO_MOVE_EVENT | INITIALIZED_EVENT
+
+	class Chess {
+		constructor(fenOrProps?: string | { fen?: string, pgn?: string });
+		/**
+		 * @returns {string} the FEN of the last move, or the setUpFen(), if no move was made.
+		 */
+		fen(move?: Move): string;
+		/**
+		 * @returns {string} the setUp FEN in the header or the default start-FEN
+		 */
+		setUpFen(): string;
+		/**
+		 * @returns {Record<string, string>} the header tags of the PGN.
+		 */
+		header(): {
+			StudyName?: string;
+			ChapterName?: string;
+			Event: string;
+			Site: string;
+			Date: string;
+			Round: string;
+			White: string;
+			Black: string;
+			Result: string;
+			UTCDate: string;
+			UTCTime: string;
+			Variant: string;
+			ECO: string;
+			Opening: string;
+			Annotator?: string;
+			FEN: string;
+			SetUp?: string;
+			Orientation: 'white' | 'black';
+			ChapterMode: string;
+		};
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is over at that move
+		 */
+		gameOver(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in draw at that move
+		 */
+		inDraw(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in statemate at that move
+		 */
+		inStalemate(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in draw, because of unsufficiant material at that move
+		 */
+		insufficientMaterial(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in draw, because of threefold repetition at that move
+		 */
+		inThreefoldRepetition(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in checkmate at that move
+		 */
+		inCheckmate(move?: Move): boolean;
+		/**
+		 * @param move optional
+		 * @returns {boolean} true, if the game is in check at that move
+		 */
+		inCheck(move?: Move): boolean;
+		/**
+		 * cm-chess uses cm-pgn for the history and header. See https://github.com/shaack/cm-pgn
+		 * @returns {Object[]} the moves of the game history
+		 */
+		history(): Move[];
+		/**
+		 * @returns {null|move} the last move of the main variation or `null`, if no move was made
+		 */
+		lastMove(): Move | null;
+		/**
+		 * Load a FEN
+		 * @param fen
+		 */
+		load(fen: string): void;
+		pgn: string;
+		/**
+		 * Load a PGN with variations, NAGs, header and annotations. cm-chess uses cm-pgn
+		 * fot the header and history. See https://github.com/shaack/cm-pgn
+		 * @param pgn
+		 */
+		loadPgn(pgn: string): void;
+		/**
+		 * Make a move in the game.
+		 * @param move
+		 * @param previousMove optional, the previous move (for variations)
+		 * @param sloppy to allow sloppy SAN
+		 * @returns {{}|null}
+		 */
+		move(move: Partial<Move> | string, previousMove?: Move, sloppy?: boolean): Move | null;
+		/**
+		 * Return all valid moves
+		 * @param options {{ square: "e2", piece: "n", verbose: true }}
+		 * Fields with { verbose: true }
+		 * - `color` indicates the color of the moving piece (w or b).
+		 * - `from` and `to` fields are from and to squares in algebraic notation.
+		 * - `piece`, `captured`, and `promotion` fields contain the lowercase representation of the applicable piece (pnbrqk). The captured and promotion fields are only present when the move is a valid capture or promotion.
+		 * - `san` is the move in Standard Algebraic Notation (SAN).
+		 * - `flags` field contains one or more of the string values:
+		 *      n - a non-capture
+		 *      b - a pawn push of two squares
+		 *      e - an en passant capture
+		 *      c - a standard capture
+		 *      p - a promotion
+		 *      k - kingside castling
+		 *      q - queenside castling
+		 *   A flags value of pc would mean that a pawn captured a piece on the 8th rank and promoted.
+		 * @param move
+		 * @returns {{}}
+		 */
+		moves(options?: {
+			square?: Square;
+			piece?: PieceSymbol;
+			verbose?: boolean;
+		}, move?: Move): Move[];
+		/**
+		 * Don't make a move, just validate, if it would be a correct move
+		 * @param move
+		 * @param previousMove optional, the previous move (for variations)
+		 * @param sloppy to allow sloppy SAN
+		 * @returns the move object or null if not valid
+		 */
+		validateMove(move: Move, previousMove?: Move, sloppy?: boolean): Move | null;
+		/**
+		 * Render the game as PGN with header, comments and NAGs
+		 * @param renderHeader optional, default true
+		 * @param renderComments optional, default true
+		 * @param renderNags optional, default true
+		 * @returns {string} the PGN of the game.
+		 */
+		renderPgn(renderHeader?: boolean, renderComments?: boolean, renderNags?: boolean): string;
+		/**
+		 * Get the position of the specified figures at a specific move
+		 * @param type "p", "n", "b",...
+		 * @param color "b" or "w"
+		 * @param move
+		 * @returns {Object[]} the pieces (positions) at a specific move
+		 */
+		pieces(type?: PieceSymbol, color?: Color, move?: Move): (Piece & { square: Square })[];
+		/**
+		 * get the piece on a square
+		 * @param square
+		 * @param move
+		 * @returns {{color: any, type: any}|null}
+		 */
+		piece(square: Square, move?: Move): Piece | null;
+		/**
+		 * @returns {string} "b" or "w" the color to move in the main variation
+		 */
+		turn(): Color;
+		/**
+		 * Undo a move and all moves after it
+		 * @param move
+		 */
+		undo(move?: Move): void;
+		plyCount(): number;
+		fenOfPly(plyNumber: number): string;
+		addObserver(callback: (event: Event) => void): void;
+	}
+}
